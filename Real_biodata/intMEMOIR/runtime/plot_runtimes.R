6f9ae2d76b2090368980f83a1d7c075ba0c32cdb
gillichu <gillichu@berkeley.edu>
2024-01-20 01:33:08 -0500

    Adding updated scripts and plots for round 1 pre-biorxiv version.

--- Real_biodata/intMEMOIR/runtime/plot_runtimes.R
+++ Real_biodata/intMEMOIR/runtime/plot_runtimes.R
@@ -4,27 +4,58 @@ require(ggplot2)
 
 d = read.table("merged_runtimes.txt",header=T)
 
+head(d)
+
 # Melt the data frame into long format for ggplot
 library(reshape2)
-df <- melt(d, id.vars = "Sample")
+d <- melt(d, id.vars = "Sample")
 
+head(d)
+dim(d)
+
+df2 <- read.table("runtime.txt", header=F, sep=",")
+colnames(df2) <- c("Sample", "variable", "value")
+head(df2)
+dim(df2)
+
+result_df <- df2 %>%
+  filter(Sample %in% d$Sample)
+
+df <- rbind(d, result_df)
 head(df)
+dim(df)
+#df = merge(df, df2, by="Sample", all=FALSE)
+#head(df)
+#dim(df)
 
-ggplot(df,aes(x=variable,y=value/60,color=variable)) +
+df <- subset(df, variable != "sc.MAIL")
+df
+
+df$variable <- factor(df$variable, levels = c("cass_greedy", "cass_hybrid", "startlenni", "scmail", "TiDeTree"))
+library(scales)
+options(scipen=10000)
+p1 <- ggplot(df,aes(x=variable,y=value/60)) +
   stat_summary(show.legend=FALSE) + 
   geom_boxplot(show.legend=FALSE) + 
   xlab("Topology Estimation Method") +
   ylab("Runtime (m)") +
-  theme_classic() + theme(text = element_text(size=15)) + #, legend.title = element_blank(),legend.position = "bottom") + 
-  scale_x_discrete(labels = c("1" = "TiDeTree", "2" = "sc-MAIL"))
-ggsave("runtime_comparison.pdf", width=6, height=4)
+  scale_y_log10() +
+  scale_x_discrete(labels = c("cass_greedy" = "Cass-Greedy", "cass_hybrid"="Cass-Hybrid", "startlenni"="Startle-NNI", "scmail"="sc-MAIL", "TiDeTree" = "TiDeTree")) + 
+  labs(x = "", y = "Runtime (m)", title = "Topology Est. Runtime (intMEMOIR-subset)") + 
+  theme_classic() + theme(text = element_text(size=15)) 
 
-ggplot(df, aes(x = variable, y = value/60, color = variable)) +
-  stat_summary(show.legend = FALSE) + 
-  geom_boxplot(show.legend = FALSE) + 
+df2$variable <- factor(df2$variable, levels = c("cass_greedy", "cass_hybrid", "startlenni", "scmail"))
+p2 <- ggplot(df2,aes(x=variable,y=value/60)) +
+  stat_summary(show.legend=FALSE) + 
+  geom_boxplot(show.legend=FALSE) + 
   xlab("Topology Estimation Method") +
   ylab("Runtime (m)") +
-  theme_classic() +
-  theme(text = element_text(size = 15)) +
-  scale_x_discrete(labels = c("TiDeTree", "sc-MAIL"))
-ggsave("intmemoir_runtime_comparison.pdf", width = 6, height = 4)
+  scale_y_log10() +
+  scale_x_discrete(labels = c("cass_greedy" = "Cass-Greedy", "cass_hybrid"="Cass-Hybrid", "startlenni"="Startle-NNI", "scmail"="sc-MAIL", "TiDeTree" = "TiDeTree")) + 
+  labs(x = "", y = "Runtime (m)", title = "Topology Est. Runtime (intMEMOIR-full)") + 
+  theme_classic() + theme(text = element_text(size=15)) 
+
+library(gridExtra)
+stacked_plot <- grid.arrange(p1, p2, ncol = 1)
+ggsave("intmemoir_runtime_comparison.pdf", stacked_plot, width = 6, height = 5)
+
--- Real_biodata/intMEMOIR/topology/plot_intmemoir.R
+++ Real_biodata/intMEMOIR/topology/plot_intmemoir.R
@@ -12,16 +12,109 @@ length(d$Sample)
 #head(d[order(d$NumNodes, decreasing=TRUE),], 10)
 
 tidetree_results = read.table("all_pubtrees_scores.txt", sep=",", header=T)
-samples = unique(tidetree_results$sample)
-d = d[d$Sample %in% samples,]
-d
+#samples = unique(tidetree_results$sample)
+#d = d[d$Sample %in% samples,]
+#d
 length(d$Sample)
+
+colnames(tidetree_results) <- c("Sample", "Method", "TideTreePub_RF")
+tidetree_results$Method <- NULL
+#tidetree_results$Sample <- tidetree_results$sample
+#tidetree_results$TiDeTree_RF <- tidetree_results$rf
+all_df <- merge(d, tidetree_results, by="Sample")
+head(all_df)
+
+# head(d)
+d <- all_df[order(all_df$NumNodes), ]
+df <- data.frame(d$Sample, d$NumNodes, as.numeric(d$Startle_RF), as.numeric(d$CassH_RF), as.numeric(d$Problin_RF), as.numeric(d$TideTreePub_RF)) 
+colnames(df) <- c("Sample", "NumNodes", "Startle-NNI", "Cassiopeia", "Problin", "TiDeTree (Pub)")
+#df <- na.omit(df)
+dfm <- melt(df, id.vars=c("Sample", "NumNodes"))
+head(dfm)
+p1 <- ggplot(dfm, aes(x=NumNodes, y=value, color=variable))  + 
+  #geom_boxplot() +
+  #geom_line() +
+  geom_smooth(method = "loess", se = FALSE, span = 0.3) +
+  stat_summary(alpha=0.5) +
+  ylab("RF Error") + 
+  xlab("Number of Cells") +
+  labs(color="Method", title="intMEMOIR Topology RF Comparison") + 
+  theme_classic() #+ 
+  #theme(legend.position = c(0.9, 0.3), legend.justification = c(1, 1)) # Adjust the position  
+#ggsave("intmemoir_all_rfcmp_across_numnodes.pdf", width=8, height=5)
+
+# compare RF error, but a second plot on the bottom comparing the WPS or LLH
+df <- data.frame(d$Sample, d$NumNodes, as.numeric(d$Startle_NLLH), as.numeric(d$CassH_NLLH), as.numeric(d$Problin_NLLH)) #, as.numeric(d$TideTreePub_RF)) 
+colnames(df) <- c("Sample", "NumNodes", "Startle-NNI", "Cassiopeia", "Problin") #, "TiDeTree (Pub)")
+dfm <- melt(df, id.vars=c("Sample", "NumNodes"))
+head(dfm)
+p2 <- ggplot(dfm, aes(x=NumNodes, y=value, color=variable))  + 
+  #geom_boxplot() +
+  #geom_line() +
+  geom_smooth(method = "loess", se = FALSE, span = 0.3) +
+  stat_summary(alpha=0.5) +
+  ylab("NLLH") + 
+  xlab("Number of Cells") +
+  labs(color="Method", title="intMEMOIR Topology NLLH Comparison") + 
+  theme_classic() #+ 
+  #theme(legend.position = c(0.9, 0.3), legend.justification = c(1, 1)) # Adjust the position  
+
+df <- data.frame(d$Sample, d$NumNodes, as.numeric(d$Startle_WPS), as.numeric(d$CassH_WPS), as.numeric(d$Problin_WPS)) #, as.numeric(d$TideTreePub_RF)) 
+colnames(df) <- c("Sample", "NumNodes", "Startle-NNI", "Cassiopeia", "Problin") #, "TiDeTree (Pub)")
+dfm <- melt(df, id.vars=c("Sample", "NumNodes"))
+head(dfm)
+p3 <- ggplot(dfm, aes(x=NumNodes, y=value, color=variable))  + 
+  #geom_boxplot() +
+  #geom_line() +
+  geom_smooth(method = "loess", se = FALSE, span = 0.3) +
+  stat_summary(alpha=0.5) +
+  ylab("WPS") + 
+  xlab("Number of Cells") +
+  labs(color="Method", title="intMEMOIR Topology WPS Comparison") + 
+  theme_classic() #+ 
+  #theme(legend.position = c(0.9, 0.3), legend.justification = c(1, 1)) # Adjust the position  
+
+
+# Arrange plots in a grid using gridExtra library
+library(gridExtra)
+
+# Arrange the plots in a grid
+grid_plots <- grid.arrange(p1, p2, p3, nrow = 3)
+grid_plots
+
+# compare topology via RF distance between the different methods
+d2 = read.table("/Users/gc3045/sc-mail-experiments/Real_biodata/intMEMOIR/topology/intMemoir_cmp_topology.txt", sep=",",header=T)
+head(d2)
+d2 <- d2[order(d2$NumNodes), ]
+df <- data.frame(d2$Sample, d2$NumNodes, as.numeric(d2$Startle_RF_Problin), as.numeric(d2$CassH_RF_Problin), as.numeric(d2$TideTreePub_RF_Problin)) 
+colnames(df) <- c("Sample", "NumNodes", "Startle-NNI", "Cassiopeia", "TiDeTree (Pub)")
+#df <- na.omit(df)
+tail(df)
+dfm <- melt(df, id.vars=c("Sample", "NumNodes"))
+head(dfm)
+p4 <- ggplot(dfm, aes(x=NumNodes, y=value, color=variable))  + 
+  #geom_boxplot() +
+  #geom_line() +
+  geom_smooth(method = "loess", se = FALSE, span = 0.3) +
+  stat_summary(alpha=0.5) +
+  ylab("RF Distance from sc-MAIL") + 
+  xlab("Number of Cells") +
+  labs(color="Method") +#, title="intMEMOIR Topology RF Comparison") + 
+  theme_classic() 
+grid_plots <- grid.arrange(p1, p4, nrow = 2)
+ggsave("/Users/gc3045/sc-mail-experiments/Real_biodata/intMEMOIR/topology/intmemoir_all_rfcmp_across_numnodes.pdf", grid_plots, width=8, height=5)
+
+
+
+
 
 
 tidetree_results
 tidetree_results$rf <- as.numeric(tidetree_results$rf)
 #tidetree_indep = tidetree_results[tidetree_results$method == "indep",]
 tidetree_pub = tidetree_results[tidetree_results$method == "pub",]
+
+
 
 # plot RF
 df <- data.frame(d$Sample, d$NumNodes, as.numeric(d$Startle_RF), as.numeric(d$CassH_RF), as.numeric(d$Problin_RF), as.numeric(tidetree_pub$rf)) #, d$NumNodes)
