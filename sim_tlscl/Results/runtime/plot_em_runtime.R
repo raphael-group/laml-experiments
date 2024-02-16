2a406db33e8424a7c14422c0d2b97724a855e1cf
gillichu <gillichu@berkeley.edu>
2024-02-16 11:11:37 -0500

    Updated figures, andding some runtime results for NJ, em runtime breakdown, cassilp.

--- sim_tlscl/Results/runtime/plot_em_runtime.R
+++ sim_tlscl/Results/runtime/plot_em_runtime.R
@@ -36,7 +36,12 @@ slope_estep <- coef(lm_fit_estep)[2]
 intercept_estep <- coef(lm_fit_estep)[1]
 
 #median(d[d$size == 5000,]$estep, na.rm=TRUE)
+install.packages("latex2exp")
+library(latex2exp)
 
+e1 <- expression(Mstep:~nu==0)
+e2 <- expression(Mstep:~nu>0)
+e3 <- expression(Estep)
 cbPalette <- c("#000000", "#E69F00", "gray", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 sizes <- sort(unique(d$size))
 # I want to add std deviation lines for each one
@@ -52,7 +57,7 @@ p <- ggplot(d, aes(x = size)) + #, color=flag)) +
   labs(x = "N*k", y = "runtime (s)", title = "Average EM Runtime Log-log Breakdown") + 
   guides(color = guide_legend(nrow = 2), shape = guide_legend(nrow = 2, override.aes = list(color = c("black", "black")))) + 
   theme_bw() +
-  scale_colour_manual(name = "Cases", values = cbPalette, labels = c("M-step Convex (Non-Heritable)", "E-step", "M-step Non-convex (Heritable)")) + 
+  scale_colour_manual(name = "Cases", values = cbPalette, labels = c(e1, e3, e2)) + 
   scale_x_continuous(expand = c(0.05, 0.1), trans='log10', labels = function(x) paste(x,"*", "30", "=", x*30) ) +
   scale_y_continuous(trans='log10') + 
   theme(legend.position = "bottom") +
@@ -67,34 +72,56 @@ ggsave("simtlscl_em_runtime_comparison.pdf", p, width=7, height=4)
 
 d_meta = read.table("meta_total_varycells.txt",header=T,sep=',')
 head(d_meta)
+
+lm_fit_noheritable <- lm(log10(totaltime) ~ log10(size), data = subset(d_meta, flag == "closed-form"))
+slope_noheritable <- coef(lm_fit_noheritable)[2]
+intercept_noheritable <- coef(lm_fit_noheritable)[1]
+
+lm_fit_heritable <- lm(log10(totaltime) ~ log10(size), data = subset(d_meta, flag == "non-convex"))
+slope_heritable <- coef(lm_fit_heritable)[2]
+intercept_heritable <- coef(lm_fit_heritable)[1]
+
 p1 <- ggplot(d_meta, aes(x=size, y=totaltime)) +
   stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag)) + 
-  stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag), geom="line") + 
+  geom_smooth(data = subset(d_meta, flag == "closed-form"), aes(color=flag), method = "lm", se = FALSE) +
+  geom_smooth(data = subset(d_meta, flag == "non-convex"), aes(color=flag), method = "lm", se = FALSE) + 
+  #stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag), geom="line") + 
   stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag)) +
-  stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag), geom="line") +
-  scale_color_manual(values = c("#1f78b4", "#33a02c"), labels=c("Convex (Non-heritable)", "Non-convex (Heritable)")) +  # Blue and green
-  scale_y_continuous(trans='log10') + 
+  #stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag), geom="line") +
+  scale_color_manual(values = c("#1f78b4", "#33a02c"), labels=c(expression(nu==0), expression(nu>0))) +  # Blue and green
+  scale_y_continuous(trans='log10') + scale_x_continuous(trans='log10') + 
+  geom_text(aes(x=500, y=1000, label=sprintf("y=%.2fx%.2f", slope_heritable, intercept_heritable)), color= "#33a02c") +
+  geom_text(aes(x=500, y=10, label=sprintf("y=%.2fx%.2f", slope_noheritable, intercept_noheritable)), color="#1f78b4") +
   ylab("Runtime of Branch Length Estimation (s)") +
   xlab("# Cells") +
   labs(color = "Cases") +
   theme_bw() +   theme(legend.position = "bottom")
+p1
 
-
-p2 <-ggplot(d_meta, aes(x=size, y=totalnumiter)) +
-  stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag)) + 
-  stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag), geom="line") + 
-  stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag)) +
-  stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag), geom="line") +
-  scale_color_manual(values = c("#1f78b4", "#33a02c"), labels=c("Convex (Non-heritable)", "Non-convex (Heritable)")) +  # Blue and green
+p2 <-ggplot(d_meta, aes(x=size, y=totalnumiter,color=flag)) + # as.factor(size)
+  #stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag)) + 
+  #stat_summary(data=subset(d_meta, flag=="closed-form"), aes(color=flag), geom="line") + 
+  #stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag)) +
+  #stat_summary(data=subset(d_meta, flag=="non-convex"), aes(color=flag), geom="line") +
+  #stat_summary(geom="line") +
+  stat_summary() + 
+  stat_summary(geom="line") + 
+  #geom_boxplot() + 
+  #stat_summary(position=position_dodge(width=0.8)) + 
+  scale_color_manual(values = c("#1f78b4", "#33a02c"), labels=c(expression(nu==0), expression(nu>0))) +  # Blue and green
   #scale_y_continuous(trans='log10') + 
+  scale_x_log10() + scale_y_log10() + 
   ylab("# EM Iterations") +
   xlab("# Cells") +
   labs(color = "Cases") +
   theme_bw() +   theme(legend.position = "bottom")
-
+p2
 library(ggpubr)
 combined_plot <- ggarrange(p1, p2, common.legend=TRUE, legend="bottom", ncol = 2)
 
-ggsave("simtlscl_em_meta.pdf", combined_plot, width=7, height=4)
+#install.packages('Cairo')
+#library(Cairo)
+
+ggsave("simtlscl_em_meta.pdf", combined_plot, width=7, height=4) #, type="cairo")
 
 
--- sim_tlscl/Results/runtime/plot_topology_runtime.R
+++ sim_tlscl/Results/runtime/plot_topology_runtime.R
@@ -1,24 +1,106 @@
 setwd('/Users/gc3045/sc-mail-experiments/sim_tlscl/Results/runtime')
 
+time_to_seconds <- function(time_str) {
+  minutes <- as.numeric(regmatches(time_str, regexpr("\\d+(?=m)", time_str, perl = TRUE)))
+  seconds <- as.numeric(regmatches(time_str, regexpr("\\d+\\.\\d+(?=s)", time_str, perl = TRUE)))
+  
+  if (length(minutes) > 0) {
+    total_seconds <- minutes * 60 + seconds
+  } else {
+    total_seconds <- seconds
+  }
+  
+  return(total_seconds)
+}
+
 require(ggplot2)
 
 d = read.table("topology_runtime_results.csv",header=T,sep=',')
-d$TotalSeconds <- as.numeric(d$TotalSeconds)
+nj_runtime = read.table("nj_simtlscl_runtime.txt", header=T, sep=",")
+nj_runtime <- nj_runtime %>%
+  mutate(TotalSeconds = sapply(Runtime, time_to_seconds))
 
-d$method = factor(d$Method,levels=c("cassgreedy","scmail","startle"),labels = c("Cassiopeia-Greedy","sc-MAIL","Startle-NNI"))
 
-
+head(nj_runtime)
 head(d)
+d$TotalSeconds <- as.numeric(d$TotalSeconds)
+nj_runtime$TotalSeconds <- as.numeric(nj_runtime$TotalSeconds)
 
-ggplot(d,aes(x=method,y=TotalSeconds/60,color=method)) +
+d <- rbind(d, nj_runtime)
+dim(d)
+#time_to_seconds("1m11.820s")
+
+d$method = factor(d$Method,levels=c("nj", "cassgreedy","scmail","startle"),labels = c("Neighbor-\nJoining", "Cassiopeia-\nGreedy","Startle-NNI","sc-MAIL"))
+
+df <- d %>%
+  mutate(total_time = ifelse(method == "Startle-NNI", TotalSeconds[which(method == "Startle-NNI")] + TotalSeconds[which(method == "Cassiopeia-Greedy")], TotalSeconds))
+head(df)
+df2 <- df %>%
+  mutate(total_time = ifelse(method == "sc-MAIL", total_time[which(method == "Startle-NNI")] + total_time[which(method == "sc-MAIL")], total_time))
+head(df2)
+
+scmail_df <- subset(df2, method == "sc-MAIL")
+startle_df <- subset(df2, method == "Startle-NNI")
+cassg_df <- subset(df2, method == "Cassiopeia-\nGreedy")
+head(cassg_df)
+head(startle_df)
+head(scmail_df)
+
+p1 <- ggplot(df2,aes(x=method,y=TotalSeconds/60)) +
   stat_summary() + 
   geom_boxplot() + 
+  scale_y_log10() + 
   # geom_smooth() + 
-  xlab("Topology Estimation Method") +
-  ylab("Runtime (m)") +
+  xlab("") +
+  labs(x = "", y = "Runtime (m)", title = "Topology Est. Runtime\n(All model conditions)") + 
   scale_color_discrete(guide = FALSE) +
   theme_classic() + theme(text = element_text(size=15)) #, legend.title = element_blank(),legend.position = "bottom") #+ 
   #scale_x_discrete(labels = c("1" = "Cassiopeia-Greedy", "2" = "sc-MAIL", "3" = "Startle-NNI"))
 # ggsave("brlen_ratio_trueTopo.pdf",width=6,height=4)
 ggsave("simtlscl_topology_runtime_comparison.pdf", width=6, height=4)
+
+library(scales)
+options(scipen=10000)
+
+
+########################################
+require('dplyr')
+d2 = read.table("cassilp_runtime_results.csv",header=T,sep=',')
+d2$TotalSeconds <- as.numeric(d2$TotalSeconds)
+d2 <- d2[!is.na(d2$TotalSeconds), ]
+dim(d2)
+head(d2)
+
+d2$method <- "Cassiopeia-ILP"  # Adding the 'method' column to d2
+colnames(d2) <- c("Sample", "TotalSeconds", "method")
+#d1 <- d[, c("Sample", "TotalSeconds", "method")]
+d1 <- df2[, c("Sample", "TotalSeconds", "method")]
+head(d1)
+
+head(d2)
+# Assuming 'd2' is your dataframe
+# Remove NA values from TotalSeconds column
+
+
+d1_filtered <- d1[d1$Sample %in% d2$Sample, ]
+merged_df <- rbind(d1_filtered, d2)
+merged_df$Method = factor(merged_df$method,levels= c("Neighbor-Joining", "Cassiopeia-Greedy","Startle-NNI","sc-MAIL", "Cassiopeia-ILP"), labels = c("Neighbor-\nJoining", "Cassiopeia-\nGreedy","Startle-NNI","sc-MAIL","Cassiopeia-\nILP"))
+
+p2 <- ggplot(merged_df,aes(x=method,y=TotalSeconds/60)) +
+  stat_summary() + 
+  geom_boxplot() + 
+  scale_y_log10() + 
+  # geom_smooth() + 
+  xlab("") +
+  labs(x = "", y = "Runtime (m)", title = "Topology Est. Runtime\n(All model conditions)") + 
+  scale_color_discrete(guide = FALSE) +
+  theme_classic() + theme(text = element_text(size=15))
+
+
+library(gridExtra)
+stacked_plot <- grid.arrange(p1, p2, ncol = 1)
+
+ggsave("simtlscl_topology_runtime_comparison.pdf", stacked_plot, width=8, height=6)
+
+
 
